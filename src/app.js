15d13e874618a2a0ac21276c656554fcb653fa81
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-27 07:03:47 +0600

    origin updated

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use(cors({
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
 
 app.use(express.json({ limit: "10mb" }));
